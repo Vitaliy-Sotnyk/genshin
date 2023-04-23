3c299e6cbea5f02142cfd3256195f81502538542
Vitaliy-Sotnyk <otaman.19.20.70@gmail.com>
2023-04-23 22:25:18 +0300

    Make base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import data from './assets/characterData.json'
-import './App.scss'
+import { useState } from "react"
+import data from "./assets/characterData.json"
+import "./App.scss"
 
 function App() {
   // const [characterInfo, setCharacterInfo] = useState();
   // const [buttonState, setButtonState] = useState(false);
-  const [selectResourseValue,setSelectResourseValue] = useState('Вознесіння')
+  const [selectResourseValue, setSelectResourseValue] = useState("Вознесіння")
   // const [selectionProcess, setSelectionProcess] = useState(false);
   // const changeStyle = () => {
-  //   if (buttonState) return (['active-button', 'active-list']);
-  //   return (['', '']);
+  //   if (buttonState) return (["active-button", "active-list"]);
+  //   return (["", ""]);
   // }
 
   /* */
@@ -21,25 +21,48 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className='background-video'>
-          <video className='background-video__video' src={`${dehyaInfo.element.background}`} autoPlay muted loop>
+        <div className="background-video">
+          <video className="background-video__video" src={`${dehyaInfo.element.background}`} autoPlay muted loop>
             Your browser does not support the video tag.
           </video>
         </div>
-        <header className='header'>
-          <div className='header__character'>
+        <header className="header">
+          <div className="header__character">
             <img className="header__character__full-img" src={dehyaInfo.images.full} alt={dehyaInfo.name} />
-            <div className="header__character__name">
-              <img src="../images/switch.png" alt="Button" />
-              <h1>{dehyaInfo.name}</h1>
+            <div className="header__character__name select-container">
+              <input type="checkbox" id="select-character-name-arrow" />
+              <label className="header__character__name__value  select-container__value" htmlFor="select-character-name-arrow">
+                <h2>{dehyaInfo.name}</h2>
+                <div className="header__character__name__value__image select-container__value__image">
+                  <img src="../images/switch.png" alt="Button" />
+                </div>
+              </label>
+              <ul className="header__character__name__options select-container__options">
+                <li className="select-container__options__item"
+                  onClick={() => setSelectResourseValue("Дех'я")}>
+                  <h3>Дех'я</h3>
+                  <div className={`select-container__options__item__image 
+                    ${selectResourseValue === "Дех'я" ? "options-selected" : ""}`}>
+                    <img src="../images/switch.png" alt="Button" />
+                  </div>
+                </li>
+                <li className="select-container__options__item"
+                  onClick={() => setSelectResourseValue("Юла")}>
+                  <h3>Юла</h3>
+                  <div className={`select-container__options__item__image 
+                    ${selectResourseValue === "Юла" ? "options-selected" : ""}`}>
+                    <img src="../images/switch.png" alt="Button" />
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className='header__level'>
-            <h3 className='header__level__value'> Поточний рівень:
-              <input type='' className='numerical-value' placeholder={0} />
+          <div className="header__level">
+            <h3 className="header__level__value"> Поточний рівень:
+              <input type="" className="numerical-value" placeholder={0} />
             </h3>
-            <h3 className='header__level__value'> Цільовий рівень:
-              <input type='' className='numerical-value' placeholder={90} />
+            <h3 className="header__level__value"> Цільовий рівень:
+              <input type="" className="numerical-value" placeholder={90} />
             </h3>
           </div>
         </header>
@@ -52,107 +75,121 @@ function App() {
           </ul>
         </nav> */}
         <main >
-          <section className='select-resourse'>
-            <div className='select-resourse__selected'>
-              <input type="checkbox" id='select-resourse-arrow' />
-              <label className='select-resourse__selected__image' htmlFor='select-resourse-arrow'>
+          <section className="select-resourse">
+            <div className="select-resourse__selected  select-container">
+              <input type="checkbox" id="select-resourse-arrow" />
+              <label className="select-resourse__selected__value  select-container__value" htmlFor="select-resourse-arrow">
                 <h3>{selectResourseValue}</h3>
-                <img src="../images/switch.png" alt="Button" />
+                <div className="select-resourse__selected__value__image select-container__value__image">
+                  <img src="../images/switch.png" alt="Button" />
+                </div>
               </label>
-              {/* <div className='select-resourse__selected__options'>
-                <input type="radio" id="upgrade" name/>
-                  <label id="en_language" htmlFor="upgrade">Вознесіння</label>
-                <input type="radio" id="talants"/>
-                  <label id="ua_language" htmlFor="talants">Таланти</label>
-              </div> */}
+              <ul className="select-resourse__selected__options select-container__options">
+                <li className="select-container__options__item"
+                  onClick={() => setSelectResourseValue("Вознесіння")}>
+                  <h3>Вознесіння</h3>
+                  <div className={`select-container__options__item__image 
+                    ${selectResourseValue === "Вознесіння" ? "options-selected" : ""}`}>
+                    <img src="../images/switch.png" alt="Button" />
+                  </div>
+                </li>
+                <li className="select-container__options__item"
+                  onClick={() => setSelectResourseValue("Таланти")}>
+                  <h3>Таланти</h3>
+                  <div className={`select-container__options__item__image 
+                    ${selectResourseValue === "Таланти" ? "options-selected" : ""}`}>
+                    <img src="../images/switch.png" alt="Button" />
+                  </div>
+                </li>
+              </ul>
             </div>
           </section>
-          <section className='resourse'>
-            <section className='resourse__array'>
+          <section className="resourse">
+            <section className="resourse__array">
               <h3 className="resourse__array__title resourse-title">Книги досвіду</h3>
               <div className="resourse__array__elements upgrade-materials">
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={expBooks.wanderer_book.icon} alt={expBooks.wanderer_book.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={expBooks.adventurer_book.icon} alt={expBooks.adventurer_book.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={expBooks.hero_book.icon} alt={expBooks.hero_book.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
               </div>
             </section>
-            <section className='resourse__array'>
+            <section className="resourse__array">
               <h3 className="resourse__array__title resourse-title">Кристали</h3>
               <div className="resourse__array__elements upgrade-materials">
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_sliver.icon} alt={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_sliver.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_fragment.icon} alt={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_fragment.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_chunk.icon} alt={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_chunk.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_gemstone.icon} alt={dehyaInfo.ascension_materials.ascension_gems.agnidus_agate_gemstone.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
               </div>
             </section>
-            <section className='resourse__array'>
+            <section className="resourse__array">
               <h3 className="resourse__array__title resourse-title">Лут з монстрів</h3>
               <div className="resourse__array__elements upgrade-materials">
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.enemy_drops.faded_red_satin.icon} alt={dehyaInfo.ascension_materials.enemy_drops.faded_red_satin.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.enemy_drops.trimmed_red_silk.icon} alt={dehyaInfo.ascension_materials.enemy_drops.trimmed_red_silk.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.enemy_drops.rich_red_brocade.icon} alt={dehyaInfo.ascension_materials.enemy_drops.rich_red_brocade.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
               </div>
             </section>
-            <section className='resourse__array small-arrea'>
+            <section className="resourse__array small-arrea">
               <h3 className="resourse__array__title resourse-title">Диковинка</h3>
               <div className="resourse__array__elements upgrade-materials">
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.daily_boss_drop.icon} alt={dehyaInfo.ascension_materials.daily_boss_drop.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
               </div>
             </section>
-            <section className='resourse__array small-arrea'>
+            <section className="resourse__array small-arrea">
               <h3 className="resourse__array__title resourse-title">Лут з боса</h3>
               <div className="resourse__array__elements upgrade-materials">
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.local_specialty.icon} alt={dehyaInfo.ascension_materials.local_specialty.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
               </div>
             </section>
-            <section className='resourse__array small-arrea'>
+            <section className="resourse__array small-arrea">
               <h3 className="resourse__array__title resourse-title">Мора</h3>
               <div className="resourse__array__elements upgrade-materials">
-                <div className='upgrade-materials__element'>
+                <div className="upgrade-materials__element">
                   <img className="upgrade-materials__element__backgroung" src={dehyaInfo.ascension_materials.mora.icon} alt={dehyaInfo.ascension_materials.mora.name} />
-                  <input type='' className='numerical-value' placeholder={0} />
+                  <input type="" className="numerical-value" placeholder={0} />
                 </div>
               </div>
             </section>
           </section>
-          <section className='calculate'>
-            <button className='calculate__button'>Розрахувати</button>
+          <section className="calculate">
+            <button className="calculate__button">Розрахувати</button>
           </section>
         </main>
       </div>

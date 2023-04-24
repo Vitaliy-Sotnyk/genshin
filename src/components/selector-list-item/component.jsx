d956c520ef93dfe5594750a8b625b90378be29ae
olha.hoshko <olha.hoshko.w@gmail.com>
2023-04-24 20:51:41 +0300

    changed selector-list-item component

--- src/components/selector-list-item/component.jsx
+++ src/components/selector-list-item/component.jsx
@@ -6,29 +6,24 @@ export const SelectorListItem = ({ itemName, selectorType }) => {
   const { handleListItemClick, selectCharacter } = useCharacterSelectorContext();
   const { handleLevelingOptionClick, selectLevelingOptionValue } = useLevelingOptionsContext();
   return (
-    <>
+    <li className="select-container__options__item"
+      onClick={() => selectorType === SelectorTypes.characterSelector
+        ? handleListItemClick(itemName)
+        : handleLevelingOptionClick(itemName)
+      }>
+      <h3>{itemName}</h3>
       {
         selectorType === SelectorTypes.characterSelector &&
-        <li className="select-container__options__item"
-          onClick={() => handleListItemClick(itemName)}>
-          <h3>{itemName}</h3>
-          <div className={`select-container__options__item__image 
-                    ${selectCharacter === itemName ? "options-selected" : ""}`}>
-            <img src="../images/switch.png" alt="Button" />
-          </div>
-        </li>
+        <div className={`select-container__options__item__image ${selectCharacter === itemName ? "options-selected" : ""}`}>
+          <img src="../images/switch.png" alt="Button" />
+        </div>
       }
       {
         selectorType === SelectorTypes.levelingOptionsSelector &&
-        <li className="select-container__options__item"
-          onClick={() => handleLevelingOptionClick(itemName)}>
-          <h3>{itemName}</h3>
-          <div className={`select-container__options__item__image 
-                    ${selectLevelingOptionValue === itemName ? "options-selected" : ""}`}>
-            <img src="../images/switch.png" alt="Button" />
-          </div>
-        </li>
+        <div className={`select-container__options__item__image ${selectLevelingOptionValue === itemName ? "options-selected" : ""}`}>
+          <img src="../images/switch.png" alt="Button" />
+        </div>
       }
-    </>
+    </li>
   );
 };
